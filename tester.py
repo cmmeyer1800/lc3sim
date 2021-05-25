cfaf951852aaaec538fb251fafa3e86596e5ff90
collin <collinmmeyer@gmail.com>
2021-05-25 12:23:15 -0500

    Created python testing script

---
+++ tester.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+import os
+
+def errCheck(test):
+    result = subprocess.run(test[0],capture_output=True, text=True)
+    if result.stdout == test[1]:
+        return True
+    else:
+        return False
+
+def main():
+    os.system('g++ -o lc3sim lc3sim.cpp')
+    testList = [(['./lc3sim', 'tests/code.obj'], 'ABCDE\n'), (['./lc3sim', 'tests/code2.obj'], 'A\n')]
+    for i in range(len(testList)):
+        if errCheck(testList[i]):
+            print("test:", i, "passed")
+        else:
+            print("test", i, "failed")
+
+if __name__ == "__main__":
+    main()
